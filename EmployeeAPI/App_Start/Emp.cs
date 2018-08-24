197d41fd3407c16e1047f4a1492266c642bbaa7c
admin <admin@ADMIN-PC>
2018-08-24 07:58:24 -0700

    Add project files.

---
+++ EmployeeAPI/App_Start/Emp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeAPI.App_Start
+{
+    public class Emp
+    {
+        public string id { get; set; } 
+        public string ApplicationId { get; set; }
+        public string Type { get; set; }
+        public string Summary { get; set; }
+        public double Amount { get; set; }
+        public string PostingDate { get; set; }
+        public Boolean IsCleared { get; set; }
+        public string ClearedDate { get; set; }
+    }
+}
---
+++ EmployeeAPI/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+
+        [HttpPost]
+       public bool AddEmpdetails()
+        {
+
+            return 1000;
+
+
+            return true;
+        }
+
+        [HttpGet]
+        public EmployeeAPI.App_Start.Emp GetEmpdetails()
+        {
+            return new EmployeeAPI.App_Start.Emp()
+            {
+                id = "3f2b12b8-2a06-45b4-b057-45949279b4e5",
+                ApplicationId = "197104",
+                Type = "Debit",
+                Summary= "Payment",
+                Amount= 58.26,
+                PostingDate = "2016 - 07 - 01T00:00:00",
+                IsCleared = true,
+                ClearedDate = "2016-07-02T00:00:00"
+            };
+        }
+
+        [HttpDelete]
+        public string DeleteEmpdetails(string id)
+        {
+            return "Deleted Employee "+ id;
+        }
+
+        [HttpPut]
+        public  string updateEmpdetails(string id, string ApplicationId,string Type,string Summary,float Amount,DateTime PostingDate, string IsCleared, DateTime ClearedDate)
+        {
+            return "Employee details updated";
+        }
+    }
+}
